710ae4fbd7e89b64de481dd2357154ea8abb5b9e
khemraj <piprodiakhemraj@gmail.com>
2026-03-03 18:47:37 +0530

    fix: explicit app export and vercel paths

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ import connectDB from "./db/index.js";
 const PORT = process.env.PORT || 5000;
 
 connectDB()
-.then(() => {
-  module.exports = app;
-})
-.catch((err) => {
-  console.log("MongoDb connection failed !!!" , err)
-})
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running at port : ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MongoDb connection failed !!! ", err);
+    });
+
+// Move the export out here so Vercel can find it immediately
+export default app;
